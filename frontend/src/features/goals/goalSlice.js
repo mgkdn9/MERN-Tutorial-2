76c121ffbc7098d30046176affab094a0203bfe8
Mike <mgkdn9@gmail.com>
2023-06-02 12:36:35 -0500

    shows goals and can delete them. done

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -12,18 +12,61 @@ const initialState = {
 
 // Create new goal
 // goalData being passed in is just 'text' state variable in GoalForm.jsx
-export const createGoal = createAsyncThunk("goals/create", async (goalData, thunkAPI) => {
-  try {
-    const token = thunkAPI.getState().auth.user.token
-    return await goalService.createGoal(goalData, token)
-  } catch (error) {
-    const message =
-      (error.response && error.response.data && error.response.data.message) ||
-      error.message ||
-      error.toString();
-    return thunkAPI.rejectWithValue(message);
+export const createGoal = createAsyncThunk(
+  "goals/create",
+  async (goalData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await goalService.createGoal(goalData, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
   }
-});
+);
+
+// Get user goals
+export const getGoals = createAsyncThunk(
+  "goals/getAll",
+  async (_, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await goalService.getGoals(token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+// Delete user goal
+export const deleteGoal = createAsyncThunk(
+  "goals/delete",
+  async (id, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await goalService.deleteGoal(id, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const goalSlice = createSlice({
   name: "goal",
@@ -36,20 +79,49 @@ export const goalSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(createGoal.pending, (state) => {
-        state.isLoading = true
+        state.isLoading = true;
       })
       .addCase(createGoal.fulfilled, (state, action) => {
-        state.isLoading = false
-        state.isSuccess = true
+        state.isLoading = false;
+        state.isSuccess = true;
         //api sends back new goal as 'action.payload'
-        state.goals.push(action.payload)
+        state.goals.push(action.payload);
       })
       .addCase(createGoal.rejected, (state, action) => {
-        state.isLoading = false
-        state.isError = true
-        state.message = action.payload
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
       })
-  }
+      .addCase(getGoals.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getGoals.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        //api sends back goals as 'action.payload'
+        state.goals = action.payload;
+      })
+      .addCase(getGoals.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(deleteGoal.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteGoal.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        //api sends back deleted goal's id as 'action.payload'
+        //so we filter deleted goal out of goals state array using id
+        state.goals = state.goals.filter((goal) => goal._id !== action.payload.id);
+      })
+      .addCase(deleteGoal.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
+  },
 });
 
 export const { reset } = goalSlice.actions;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,19 +1,39 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from "../components/GoalForm";
+import Spinner from '../components/Spinner'
+import { getGoals, reset } from "../features/goals/goalSlice";
+import GoalItem from '../components/GoalItem'
 
 function Dashboard() {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   //useSelector always takes in function that takes in state and here assigns 'user' to 'auth' prop of state. It 'selects' which global state variable you want (auth) to be loaded into the local variable you're creating (user)
   const {user} = useSelector((state) => state.auth)
+  const {goals, isLoading, isError, message} = useSelector((state) => state.goals)
 
   useEffect(() => {
+    if(isError) {
+      console.log(message)
+    }
+
     if(!user) {
       navigate('/login')
     }
-  }, [user, navigate])
+
+    dispatch(getGoals())
+
+    return () => {
+      // try adding back in if problems arise...
+      // dispatch(reset()) //return from useEffect what you want to happen if component unmounts. In this case, we want the goals to reset if they leave Dashboard
+    }
+  }, [user, navigate, isError, message, dispatch])
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (<>
     <section className="heading">
@@ -22,6 +42,16 @@ function Dashboard() {
     </section>
 
     <GoalForm />
+
+    <section className="content">
+      {goals.length > 0 ? (
+        <div className="goals">
+          {goals.map((goal) => (
+            <GoalItem key={goal._id} goal={goal} />
+          ))}
+        </div>
+      ) : (<h3>You have not set any goals</h3>)}
+    </section>
   </>);
 }
 

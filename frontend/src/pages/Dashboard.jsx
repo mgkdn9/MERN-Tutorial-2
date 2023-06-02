dceb5f37751e02718bf6319f37d8d40561b3da4c
Mike <mgkdn9@gmail.com>
2023-06-02 11:37:07 -0500

    can store goals in db from frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from 'react-redux'
+import GoalForm from "../components/GoalForm";
 
 function Dashboard() {
   const navigate = useNavigate()
 
-  //useSelector always takes in function that takes in state and here assigns 'user' to 'auth' prop of state
+  //useSelector always takes in function that takes in state and here assigns 'user' to 'auth' prop of state. It 'selects' which global state variable you want (auth) to be loaded into the local variable you're creating (user)
   const {user} = useSelector((state) => state.auth)
 
   useEffect(() => {
@@ -14,7 +15,14 @@ function Dashboard() {
     }
   }, [user, navigate])
 
-  return <div>Dashboard</div>;
+  return (<>
+    <section className="heading">
+      <h1>Welcome {user && user.name}</h1>
+      <p>Goals Dashboard</p>
+    </section>
+
+    <GoalForm />
+  </>);
 }
 
 export default Dashboard;

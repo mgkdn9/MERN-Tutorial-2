3010fafca9ccee8a2fe2a7aa6601fc30587afa11
Mike <mgkdn9@gmail.com>
2023-06-02 10:33:45 -0500

    dashboard navigates to login if not logged in

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from 'react-redux'
+
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const navigate = useNavigate()
+
+  //useSelector always takes in function that takes in state and here assigns 'user' to 'auth' prop of state
+  const {user} = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    if(!user) {
+      navigate('/login')
+    }
+  }, [user, navigate])
+
+  return <div>Dashboard</div>;
 }
 
-export default Dashboard
+export default Dashboard;
